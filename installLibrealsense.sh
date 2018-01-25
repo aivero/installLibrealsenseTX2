90f3a13d45473fbb2185e4744b2d5c47959a55d4
Rapha <compiaffe@users.noreply.github.com>
2018-01-25 15:26:09 +0000

    Add libudev

--- installLibrealsense.sh
+++ installLibrealsense.sh
@@ -9,7 +9,7 @@ sudo apt-get install libglfw3-dev -y
 sudo apt-get install qtcreator -y
 sudo apt-get install cmake-curses-gui -y
 sudo apt-get install -y qdbus qmlscene qt5-default qt5-qmake qtbase5-dev-tools qtchooser qtdeclarative5-dev xbitmaps xterm libqt5svg5-dev qttools5-dev qtscript5-dev qtdeclarative5-folderlistmodel-plugin qtdeclarative5-controls-plugin
-sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libgtk-3-dev libudev-dev
 
 # Install librealsense into home directory
 cd $HOME
