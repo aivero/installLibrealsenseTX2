27c5ffc68062cdd73a1fba83f3019dcca4992cd3
Rapha <compiaffe@users.noreply.github.com>
2018-01-25 15:15:16 +0000

    Fix installation on TX2 with librealsense at development and D435/D415

--- installLibrealsense.sh
+++ installLibrealsense.sh
@@ -8,16 +8,15 @@ sudo apt-get install libusb-1.0-0-dev pkg-config -y
 sudo apt-get install libglfw3-dev -y
 sudo apt-get install qtcreator -y
 sudo apt-get install cmake-curses-gui -y
+sudo apt-get install -y qdbus qmlscene qt5-default qt5-qmake qtbase5-dev-tools qtchooser qtdeclarative5-dev xbitmaps xterm libqt5svg5-dev qttools5-dev qtscript5-dev qtdeclarative5-folderlistmodel-plugin qtdeclarative5-controls-plugin
+
 # Install librealsense into home directory
 cd $HOME
 git clone https://github.com/IntelRealSense/librealsense.git
 cd librealsense
 # Checkout version 1.12.1 of librealsense, last tested version
-git checkout v1.12.1
-# Patch the uvcvideo internal module fix
+git checkout # Patch the uvcvideo internal module fix
 patch -p1 -i $INSTALL_DIR/patches/uvc-v4l2.patch
-# Install Qt libraries
-sudo scripts/install_qt.sh
 # Copy over the udev rules so that camera can be run from user space
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && udevadm trigger
